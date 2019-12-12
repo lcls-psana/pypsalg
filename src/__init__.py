166bdba98d6bc565c891cb43be02e6afde08127f
Aaron Brewster <asbrewster@lbl.gov>
2019-12-12 10:11:32 -0800

    2to3 absolute_import

--- src/__init__.py
+++ src/__init__.py
@@ -4,16 +4,17 @@ pypsalg:
 pypsalg is a python module of common functions used at LCLS.
 It also includes functions from psalg (used in AMI).
 """
+from __future__ import absolute_import
 
 
-from AngularIntegration import AngularIntegrator
-from CircularBuffer import CircularBuffer
-from CommonModeRemoval import CommonModeRemoval
-from EventFilter import EventFilter
-from Histogram import hist1d
-from Mask import *
-from gsc import gsc
-from AngularIntegrationM import AngularIntegratorM
+from .AngularIntegration import AngularIntegrator
+from .CircularBuffer import CircularBuffer
+from .CommonModeRemoval import CommonModeRemoval
+from .EventFilter import EventFilter
+from .Histogram import hist1d
+from .Mask import *
+from .gsc import gsc
+from .AngularIntegrationM import AngularIntegratorM
 
 # Load python interface to psalg
 from pypsalg_cpp import *
